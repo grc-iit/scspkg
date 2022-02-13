348847d36fb65c7c275f2a00693ee0667da857af
lukemartinlogan <lukemartinlogan@gmail.com>
2022-02-13 16:12:51 -0600

    Add install env-modules

--- install.sh
+++ install.sh
@@ -12,19 +12,30 @@ else
       exit
     fi
   fi
+  mkdir -p ${SCSPKG_ROOT}
+  cp -r . ${SCSPKG_ROOT}
 fi
 
-mkdir -p ${SCSPKG_ROOT}
-cp -r . ${SCSPKG_ROOT}
 echo export SCSPKG_ROOT=${SCSPKG_ROOT} >> ~/.bashrc
 echo export PATH=${SCSPKG_ROOT}/bin:"\$PATH" >> ~/.bashrc
-if ! command -v module &> /dev/null
-then
-    echo "Warning: environment modules not installed"
-else
-    echo "module use \${SCSPKG_ROOT}/modulefiles" >> ~/.bashrc
-fi
 mkdir ${SCSPKG_ROOT}/packages
 mkdir ${SCSPKG_ROOT}/modulefiles
 mkdir ${SCSPKG_ROOT}/modulefiles_json
 source ~/.bashrc
+if ! command -v module &> /dev/null
+then
+    echo "Warning: environment modules not installed, will install now"
+    scspkg create modules
+    cd `scspkg pkg-src modules`
+    curl -LJO https://github.com/cea-hpc/modules/releases/download/v4.7.1/modules-4.7.1.tar.gz
+    tar xfz modules-4.7.1.tar.gz
+    cd modules-4.7.1
+    ./configure --prefix=`scspkg pkg-root modules`
+    make
+    make install
+    echo "source \`scspkg pkg-root modules\`/init/bash" >> ~/.bashrc
+    echo "module use \`scspkg modules-path\`" >> ~/.bashrc
+    source ~/.bashrc
+else
+    echo "module use \${SCSPKG_ROOT}/modulefiles" >> ~/.bashrc
+fi
